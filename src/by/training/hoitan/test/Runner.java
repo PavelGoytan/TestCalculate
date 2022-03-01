09a437bb2eb1a9c028fe838d194839f7715c5536
Pavel Goytan <3160398@gmail.com>
2022-03-02 00:20:19 +0300

    add expression

--- src/by/training/hoitan/test/Runner.java
+++ src/by/training/hoitan/test/Runner.java
@@ -4,8 +4,10 @@ import by.training.hoitan.calculator.Calculate;
 
 public class Runner {
     public static void main(String[] args) {
-        String string = "-(((2.0-2.0*9.0-8.0/3.0)-7.0*2.0)+(2.0*3.0-(4.0)))*2.0";
-        Calculate calculate = new Calculate(string);
-        System.out.println(calculate.solveExpression());
+        String string = "-(-((2.0-2.0*9.0-8.0/3.0)-7.0*2.0)+(2.0*3.0-(4.0)))*2.0*(1-2)*(2-3)";
+        String stringA = "(2+1)*(-1)/(3-2*9)";
+        Calculate calculate = new Calculate();
+        System.out.println(calculate.solveExpression(string));
+        System.out.println(calculate.solveExpression(stringA));
     }
 }
